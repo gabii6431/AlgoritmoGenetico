405c4deb27bfff52e3ede8b45129ee454cd834a8
Giovanna Taglialegna Verola Moreira <giovanna.verola2016@gmail.com>
2019-10-29 17:10:37 -0300

    Programacao dinamica funcionando

--- ProblemaDaMochila/src/ProgramacaoDinamica/Main.java
+++ ProblemaDaMochila/src/ProgramacaoDinamica/Main.java
@@ -4,34 +4,43 @@ public class Main
 {
      public static void main(String args[])
     {
-        int qtdItens = 5;
+        int quantidadeItens = 5;
         int [] peso = {5,4,3,2,1};
         int [] valor = {4,6,5,3,1};
-        int capacidade = 10;
+        int capacidadeMochila = 10;
         
-        int memorizacao[][] = new int[capacidade+1][qtdItens+1];
+        int[][] mochila = new int[capacidadeMochila+1][quantidadeItens+1];
         
-        for (int i = 0; i < capacidade; i++) {
-            for (int j = 0; j < qtdItens; j++) {
-                if(i==0 || j==0){
-                    memorizacao[i][j] = 0;
-                } else {
-                    memorizacao[i][j] = memorizacao[i][j-1];
-                    if(peso[j-1] <= i){
-                        memorizacao[i][j] = maxNumero(memorizacao[i][j-1],memorizacao[i-peso[j-1]][j-1]+valor[j-1]);
+        for (int i = 0; i <= capacidadeMochila; ++i)
+        {
+            for (int j = 0; j <= quantidadeItens; ++j)
+            {
+                if(j == 0 || i == 0)
+                {
+                    mochila[i][j] = 0;
+                }
+                else 
+                {
+                    mochila[i][j] = mochila[i][j-1];
+                    if(peso[j-1] <= i)
+                    {
+                        mochila[i][j] = maxNumero(mochila[i][j-1],mochila[i-peso[j-1]][j-1]+valor[j-1]);
                     }
                 }
             }
         }
         
-        System.out.println("Valor max: "+memorizacao[capacidade][qtdItens]);
-        
+        System.out.println("Valor max: "+mochila[capacidadeMochila][quantidadeItens]);     
     }
      
-    public static int maxNumero(int a, int b){
-        if(a>b){
+    public static int maxNumero(int a, int b)
+    {
+        if(a>b)
+        {
             return a;
-        } else{
+        }
+        else
+        {
             return b;
         }
     }
